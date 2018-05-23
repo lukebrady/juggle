4ad767d4bb7d26cdf406e846b0035c7f7e90c2e0
Luke Brady <lbrad23105@gmail.com>
2018-05-23 12:06:53 -0400

    The circular linked list object can now be created.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- pkg/algorithms/round_robin.go
+++ pkg/algorithms/round_robin.go
@@ -1,8 +1,30 @@
 package algorithms
 
-// CircLinkedList is the base data structure that will store
+// RRLinkedList is the base data structure that will store
 // information such as which host is currently receiving traffic.
-type CircLinkedList struct {
-	current *HostObject
+type RRLinkedList struct {
+	hosts   []HostObject
+	current HostObject
 	size    uint
+}
+
+// NewRRLinkedList takes a slice of hostnames and creates the circular
+// linked list that will be used to load-balance web traffic.
+func NewRRLinkedList(hosts []string) *RRLinkedList {
+	var hostObjs []HostObject
+	// Iterate through the supplied list of hosts and create host objects.
+	for _, host := range hosts {
+		hostObjs = append(hostObjs, HostObject{
+			algorithm:   "rr",
+			connections: 0,
+			hostname:    host,
+			neighbor:    nil,
+		})
+	}
+	// Return a RRLinkedList object to be used by juggle.
+	return &RRLinkedList{
+		current: hostObjs[0],
+		hosts:   hostObjs,
+		size:    uint(len(hosts)),
+	}
 }
