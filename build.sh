f1543f63658cd85399123ee916badcb30332a6fb
Luke Brady <lbrad23105@gmail.com>
2018-05-25 15:10:06 -0400

    Starting to work on the index web server.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -o cmd/juggle main.go
+go build -o cmd/juggle ./
--- internal/web/index.go
+++ internal/web/index.go
@@ -3,3 +3,14 @@ package web
 import (
 	"net/http"
 )
+
+type Index struct {
+	protocol string
+	hosts []string
+}
+
+func IndexRoute(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		http.Get()
+	}
+}
