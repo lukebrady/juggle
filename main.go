51810151213a555d67ad1e40817f8c11629b54cc
Luke Brady <lbrad23105@gmail.com>
2018-07-23 16:44:58 -0400

    Changed web server to start on the default port 9080.:
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- main.go
+++ main.go
@@ -1,29 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.Write([]byte("Juggle v1: The programmable load-balancer."))
+	}
+}
+
 func main() {
 	// Read in the juggle configuration to be used by the program.
-	configuration, err := ReadJuggleConfig("config/juggle.conf.json")
+	configuration, err := ReadJuggleConfig("./config/juggle.conf.json")
 	if err != nil {
 		panic(err)
 	}
 	// Check which algorithm has been used in the configuration.
-	switch configuration.Algorithm {
-	case "rr":
-
-		break
-	case "ll":
-		break
-	case "pr":
-		break
-	default:
-		break
-	}
+	/*
+		switch configuration.Algorithm {
+		case "rr":
+			break
+		case "ll":
+			break
+		case "pr":
+			break
+		default:
+			break
+		}  */
 	// Create the web multiplexer.
 	server := http.NewServeMux()
+	// Handle the index route.
+	server.HandleFunc("/", indexRoute)
+	fmt.Println(configuration.Port)
 	// Listen and serve the load-balancer server.
-	http.ListenAndServe(configuration.Port, server)
+	http.ListenAndServe(":9080", server)
 }
